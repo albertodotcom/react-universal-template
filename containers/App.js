78df640664240e1214bc7569d16cda529ee5bdb1
Alberto Forni <me@albertoforni.com>
2016-04-16 17:25:37 +0100

    Add nodemon to server command

--- containers/App.js
+++ containers/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <h1>Universal App</h1>
+      <h1>Universal App in React</h1>
     );
   }
 }
